5ab1d49fecdc4ace12480e88370e4885b4852605
Aaron Foley <aaron.s.foley@gmail.com>
2015-09-20 04:24:02 +1000

    Profile Work

--- www/js/firebase.js
+++ www/js/firebase.js
@@ -1,6 +1,5 @@
 var firebase = angular.module('starter.firebase', ['firebase', 'starter.config'])
 
-
 firebase.factory("Auth", function($firebaseAuth, FIREBASE_URL) {
   var usersRef = new Firebase(FIREBASE_URL + "/users");
   return $firebaseAuth(usersRef);
